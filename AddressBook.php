9499dbfdf637bc634691f1d40c9a5b172db26fc1
TheSoftDeli <84410108+TheSoftDeli@users.noreply.github.com>
2022-12-07 10:50:32 +0100

    Add files via upload

---
+++ AddressBook.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AddressBook</title>
+
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+    <!--HTML CODE-->
+
+    <table>
+        <tr>
+            <th>Nome</th>
+            <th>Cognome</th>
+            <th>telefono</th>
+            <th>email</th>
+            <th>Città</th>
+        </tr>
+        <tr>
+            
+        </tr>
+
+    </table>
+
+
+    <!--END LANG_CODE-->
+    <!--PHP CODE-->
+    <?php 
+        //CHADIA BAMAAROUF 5L - 07/12/2022
+
+    ?>
+    <!--END LANG_CODE-->
+
+
+</body>
+</html>
